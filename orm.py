242098c4f8a1daf1700409302c3d701cf7c84265
SamBuilder <sammy_sam0402@hotmail.com>
2017-05-08 21:32:02 +0800

    day - 代码优化

--- orm.py
+++ orm.py
@@ -11,31 +11,33 @@ import aiomysql
 import logging
 logging.basicConfig(level=logging.INFO)
 
+
 def log(sql, args):
     logging.info('SQL: %s' % sql)
+
 
 async def creaet_pool(loop, **kw):
     logging.info('create database connection pool')
     global __pool
-    __pool = await aiomysql.create_pool{
-        host = kw.get('host', 'localhost'),
-        port = kw.get('port', 3306),
-        user = kw['user'],
-        password = kw['password'],
-        db = kw['db'],
-        charset = kw.get('charset', 'utf8'),
-        autocommit = kw.get('autocommit', True),
-        maxsize = kw.get('maxsize', 10),
-        minsize = kw.get('minsize', 1),
-        loop = loop
-    }
+    __pool = await aiomysql.create_pool(
+        host=kw.get('host', '10.0.0.2'),
+        port=kw.get('port', 3306),
+        user=kw['user'],
+        password=kw['password'],
+        db=kw['db'],
+        charset=kw.get('charset', 'utf8'),
+        autocommit=kw.get('autocommit', True),
+        maxsize=kw.get('maxsize', 10),
+        minsize=kw.get('minsize', 1),
+        loop=loop)
+
 
 # Select
 # 要执行SELECT语句，用select函数执行，需要传入SQL语句和SQL参数
 
+
 async def select(sql, args, size=None):
     log(sql, args)
-    global __pool
     with (await __pool) as conn:
         cur = await conn.cursor(aiomysql.DictCursor)
         await cur.execute(sql.replcae('?', '%s'), args or ())
@@ -47,6 +49,7 @@ async def select(sql, args, size=None):
         logging.info('rows returned: %s' % len(rs))
         return rs
 
+
 # SQL语句的占位符是?，而MySQL的占位符是%s，select()函数在内部自动替换;
 # 注意要始终坚持使用带参数的SQL，而不是自己拼接SQL字符串，这样可以防止SQL注入攻击。
 # 注意到yield from将调用一个子协程（也就是在一个协程中调用另一个协程）并直接获得子协程的返回结果。
@@ -56,6 +59,7 @@ async def select(sql, args, size=None):
 # 要执行INSERT、UPDATE、DELETE语句，可以定义一个通用的execute()函数；
 # 因为这3种SQL的执行都需要相同的参数，以及返回一个整数表示影响的行数
 
+
 async def execute(sql, args):
     log(sql)
     with (await __pool) as conn:
@@ -64,9 +68,10 @@ async def execute(sql, args):
             await cur.execute(sql.replace('?', '%s'), args)
             affected = cur.rowcount
             await cur.close()
-            except BaseException as e:
-                raise
-            return affected
+        except BaseException as e:
+            raise
+        return affected
+
 
 # execute()函数和select()函数所不同的是，cursor对象不返回结果集，而是通过rowcount返回结果数
 
@@ -74,65 +79,41 @@ async def execute(sql, args):
 # 有了基本的select()和execute()函数，我们就可以开始编写一个简单的ORM了。
 # 设计ORM需要从上层调用者角度来设计。
 # 我们先考虑如何定义一个User对象，然后把数据库表users和它关联起来。
-
-class Model(dict, metaclass=ModelMetaclass):
-
-    def __init__(self, **kw):
-        super(Model, self).__init__(**kw)
-
-    def __getattr__(self, key):
-        try:
-            return self[key]
-        except KeyError:
-            raise AttributeError(r"'Model' object has no attribute '%s'" % key)
-
-    def __setattr__(self, key, value):
-        self[key] = value
-
-     def getValue(self, key):
-         return getattr(self, key, None)
-        
-    def getValueOrDefault(self, key):
-        value = getattr(self, key, None)
-        if value is None:
-            field = self.__mappings__[key]
-            if field.default is not None:
-                value = field.default() if callable(field.default) else field.default
-                logging.debug('using default value for %s:%s' % (key, str(value)))
-            return value
-    
-    async def save(self):
-        args = list(map(self.getValueOrDefault, self.__fields__))
-        arhs.append(self.getValueOrDefault(self.__primary_key__))
-        rows = await execute(self.__insert__, args)
-        if rows != 1:
-            logging.warn('failed to insert record: affected worsL %s' % rows)
-            
-# Model从dict继承，所以具备所有dict的功能，同时又实现了特殊方法__getattr__()和__setattr__()，因此又可以像引用普通字段那样写
-# user['id'] => 123
-# user.id => 123
-
 # 以及Field和各种Field子类
+
+
 class Field(object):
-    
-    def __init__(self,name,column_type,primary_key,default):
+    def __init__(self, name, column_type, primary_key, default):
         self.name = name
         self.column_type = column_type
         self.primary_key = primary_key
         self.default = default
-    
+
     def __str__(self):
-        return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
+        return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type,
+                                self.name)
+
 
 # 映射varchar的StringField
 class StringField(Field):
-
-    def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+    def __init__(self,
+                 name=None,
+                 primary_key=False,
+                 default=None,
+                 ddl='varchar(100)'):
         super().__init__(name, ddl, primary_key, default)
 
-# 注意到Model只是一个基类，如何将具体的子类如User的映射信息读取出来呢？答案就是通过metaclass：ModelMetaclass
-class ModelMetaclass(type):
 
+class IntgerField(Field):
+    def __init__(self,
+                 name=None,
+                 primary_key=False,
+                 default=None,
+                 ddl='bigint(100)'):
+        super().__init__(name, ddl, primary_key, default)
+
+
+class ModelMetaclass(type):
     def __new__(cls, name, bases, attrs):
         # 排除Model类本身
         if name == 'Model':
@@ -151,7 +132,8 @@ class ModelMetaclass(type):
                 if v.primary_key:
                     # 找到主键
                     if primaryKey:
-                        raise RuntimeError('Duplicate primary key for field: %s' % k)
+                        raise RuntimeError(
+                            'Duplicate primary key for field: %s' % k)
                     primaryKey = k
                 else:
                     fields.append(k)
@@ -160,16 +142,64 @@ class ModelMetaclass(type):
         for k in mappings.keys():
             attrs.pop(k)
         escaped_fields = list(map(lambda f: '`%s`' % f, fields))
-        attrs['__mapping__'] = mappings # 保存属性和列的映射关系
+        attrs['__mapping__'] = mappings  # 保存属性和列的映射关系
         attrs['__table__'] = tableName
-        attrs['__primary_key__'] = primaryKey   # 主键属性名
-        attrs['__fields__'] = fields    # 除主属性外的属性名
+        attrs['__primary_key__'] = primaryKey  # 主键属性名
+        attrs['__fields__'] = fields  # 除主属性外的属性名
         # 构造默认的SELECT, INSERT, UPDATE和DELETE语句
-        attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
-        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, ', '.join(escaped_fields, primaryKey, create_args_string(len(escaped_fields) + 1)))
-        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
-        attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
+        attrs['__select__'] = 'select `%s`, %s from `%s`' % (
+            primaryKey, ', '.join(escaped_fields), tableName)
+        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (
+            tableName, ', '.join(escaped_fields, primaryKey,
+                                 create_args_string(len(escaped_fields) + 1)))
+        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (
+            tableName, ', '.join(
+                map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)),
+            primaryKey)
+        attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName,
+                                                                 primaryKey)
         return type.__new__(cls, name, bases, attrs)
+
+# Model从dict继承，所以具备所有dict的功能，同时又实现了特殊方法__getattr__()和__setattr__()，因此又可以像引用普通字段那样写
+# user['id'] => 123
+# user.id => 123
+class Model(dict, metaclass=ModelMetaclass):
+    def __init__(self, **kw):
+        super(Model, self).__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Model' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def getValue(self, key):
+        return getattr(self, key, None)
+
+    def getValueOrDefault(self, key):
+        value = getattr(self, key, None)
+        if value is None:
+            field = self.__mappings__[key]
+            if field.default is not None:
+                value = field.default() if callable(
+                    field.default) else field.default
+                logging.debug('using default value for %s:%s' %
+                              (key, str(value)))
+            return value
+
+    async def save(self):
+        args = list(map(self.getValueOrDefault, self.__fields__))
+        arhs.append(self.getValueOrDefault(self.__primary_key__))
+        rows = await execute(self.__insert__, args)
+        if rows != 1:
+            logging.warn('failed to insert record: affected worsL %s' % rows)
+
+
+
+# 注意到Model只是一个基类，如何将具体的子类如User的映射信息读取出来呢？答案就是通过metaclass：ModelMetaclass
 
 # 这样，任何继承自Model的类（比如User），会自动通过ModelMetaclass扫描映射关系，并存储到自身的类属性如__table__、__mappings__中
 
